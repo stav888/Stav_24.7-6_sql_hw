5377ccd74baaece8c1f86ff74748eb0bf9687eff
stav888 <44903234+stav888@users.noreply.github.com>
2025-07-26 13:54:55 +0300

    Update 3 ON DELETE CASCADE.sql

--- 3 ON DELETE CASCADE.sql
+++ 3 ON DELETE CASCADE.sql
@@ -14,3 +14,30 @@ CREATE TABLE movies (
 
 INSERT INTO directors (name) VALUES ('Spielberg');
 INSERT INTO movies (title, director_id) VALUES ('Jaws', 1);
+
+-------------
+-------------
+
+DELETE FROM directors WHERE name = 'Spielberg';
+-- מתקבלת שגיאה כי יש סרט שמקושר אליו
+
+
+DROP TABLE movies;
+
+CREATE TABLE movies (
+    movie_id INTEGER PRIMARY KEY,
+    title TEXT,
+    director_id INTEGER,
+    FOREIGN KEY(director_id) REFERENCES directors(director_id) ON DELETE CASCADE
+);
+
+
+INSERT INTO movies (title, director_id) VALUES ('Jaws', 1);
+
+
+DELETE FROM directors WHERE name = 'Spielberg';
+
+
+SELECT * FROM directors;
+SELECT * FROM movies;
+

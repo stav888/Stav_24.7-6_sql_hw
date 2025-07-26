c17ab0ccfd09ec3255ef0056e9280530138dedd1
stav888 <44903234+stav888@users.noreply.github.com>
2025-07-26 14:39:59 +0300

    Update 4 SERIAL.sql

--- 4 SERIAL.sql
+++ 4 SERIAL.sql
@@ -1,1 +1,25 @@
 -- SERIAL
+
+CREATE TABLE trainers (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+INSERT INTO trainers (id, name) VALUES
+(201, 'Erez Barak'),
+(202, 'Liat Ben-Haim'),
+(203, 'Gil Oren');
+
+
+DROP TABLE IF EXISTS trainers;
+
+CREATE TABLE trainers (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+
+INSERT INTO trainers (name) VALUES
+('Erez Barak'),
+('Liat Ben-Haim'),
+('Gil Oren');

1f2ab1b7e9f975c21db284b9e8de32e812f57ff3
stav888 <44903234+stav888@users.noreply.github.com>
2025-07-27 15:37:38 +0300

    Update 6.1.sql

--- 6.1.sql
+++ 6.1.sql
@@ -1,12 +1,32 @@
-CREATE TABLE enrollments (
+CREATE TABLE students (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    courses JSONB
+    name TEXT NOT NULL
 );
 
-INSERT INTO enrollments (name, courses) VALUES
-('Roni', '{"math": "2024-11-01", "history": "2024-11-15"}'),
-('Alon', '{"math": "2024-11-01"}');
+CREATE TABLE courses (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    course_date DATE NOT NULL
+);
+
+CREATE TABLE student_courses (
+    student_id INTEGER REFERENCES students(id),
+    course_id INTEGER REFERENCES courses(id),
+    PRIMARY KEY (student_id, course_id)
+);
+
+INSERT INTO students (name) VALUES
+('Roni'),
+('Alon');
+
+INSERT INTO courses (title, course_date) VALUES
+('math', '2024-11-01'),
+('history', '2024-11-15');
+
+INSERT INTO student_courses (student_id, course_id) VALUES
+(1, 1),
+(1, 2),
+(2, 1);
 
 
 
